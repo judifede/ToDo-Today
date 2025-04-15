cb4fa48395fab929fe68e0289c38b483a944591c
Diego Fernández <diego_ss2@hotmail.com>
2025-04-15 20:43:01 +0100

    Mensajes Token

--- backend/src/todos.py
+++ backend/src/todos.py
@@ -12,12 +12,12 @@ def token_required(f):
     def decorated(*args, **kwargs):
         token = request.headers.get('Authorization')
         if not token:
-            return jsonify({'message': 'Token is missing'}), 403
+            return jsonify({'message': 'No hay Token'}), 403
         try:
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
             current_user = data['user_id']
         except:
-            return jsonify({'message': 'Token is invalid'}), 403
+            return jsonify({'message': 'Token no es válido'}), 403
         return f(current_user, *args, **kwargs)
     return decorated
 
