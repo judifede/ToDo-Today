99eb30fc13c44b9bcd59965c809cf1463664a94a
Diego Fernández <diego_ss2@hotmail.com>
2025-04-16 18:05:55 +0100

    Nueva API

--- frontend/src/Services/config.js
+++ frontend/src/Services/config.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 const isDevelopment = import.meta.env.VITE_ENV  === 'development'
 
 const BASE_URL_API_DEV = "http://localhost:5000/api"
-const BASE_URL_API_PROD = "https://todo-today-api.onrender.com/api" 
+const BASE_URL_API_PROD = "https://todo-today-lorl.onrender.com/api" 
 const BASE_URL_AUTH_DEV = "http://localhost:5000/auth"
-const BASE_URL_AUTH_PROD = "https://todo-today-api.onrender.com/auth"
+const BASE_URL_AUTH_PROD = "https://todo-today-lorl.onrender.com/auth"
 
 const api = axios.create({
   baseURL: isDevelopment ? BASE_URL_API_DEV : BASE_URL_API_PROD
